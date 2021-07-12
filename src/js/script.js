3a07cfd17eee4d68a5ba1f01ee0605565d968c0c
Abhishek <abhisawant111111@gmail.com>
2021-07-12 09:43:51 +0530

    testimony

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,51 @@ const nav = document.querySelector('.nav');
 const ham = document.querySelector('.hamburger');
 
 navToggle.addEventListener('click', () => {
-    if(nav.style.maxHeight){
+    if (nav.style.maxHeight) {
         nav.style.maxHeight = null;
         ham.classList.remove('hamburger-open');
     }
-    else{
+    else {
         nav.style.maxHeight = nav.scrollHeight + "px";
         ham.classList.add('hamburger-open');
     }
-})
+});
+
+$(document).ready(function () {
+    $('.testimony-container').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        variableHeight: true,
+        speed: 900,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
